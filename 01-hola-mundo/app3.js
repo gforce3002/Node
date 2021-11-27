1d60135185a3cf20a86e913370175ef14ab15484
francisco ramirez <gforce3002@gmail.com>
2021-11-27 10:54:41 -0600

    seccion 3 del curso

---
+++ 01-hola-mundo/app3.js
@@ -0,0 +1,16 @@
+console.log("inicio de programa");
+
+setTimeout(()=>{
+    console.log("primer timeOut...")
+},3000);
+
+setTimeout(()=>{
+    console.log("Segundo timeOut")
+},0);
+
+setTimeout(()=>{
+    console.log("Tercer timeOut")
+},0);
+
+
+console.log("Fin de programa");
---
+++ 02-fundamentos/templeate-string.js
@@ -0,0 +1,14 @@
+const nombre = "Deedpool"
+const real = "Wade Winston"
+const normal = nombre + ' ' + real;
+
+const template = `${nombre} ${real}`
+
+console.log(normal, template)
+
+const html = `
+    <h1>Hola</h1>
+    <p>Mundo</p>
+    `;
+
+console.log(html)
