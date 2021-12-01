d075544d3a02095def50d143969a26d262e00b3c
francisco ramirez <gforce3002@gmail.com>
2021-12-01 12:12:28 -0600

    tablas de multiplicar

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,13 +1,12 @@
+const colors = require('colors')
 const {crearArchivo} = require('./helpers/multiplicar') 
+const argv = require('./config/yargs')
+
 console.clear();
 
-const tabla= 3
-//console.log(process.argv)
-const [,,arg3='base=5'] = process.argv
-const [, base=5] = arg3.split('=')
-
-console.log(base)
-crearArchivo(base)
+console.log(argv)
+console.log('la base es ', argv.base, )
+crearArchivo(argv.base, argv.listar, argv.hasta)
     .then(resp => console.log(resp))
     .catch(error => console.log(error))
 
--- 03-bases-node/helpers/multiplicar.js
+++ 03-bases-node/helpers/multiplicar.js
@@ -1,25 +1,29 @@
 const fs = require('fs')
+const colors = require('colors')
 
-const crearArchivo = async (base = 5)=>{
+const crearArchivo = async (base = 5, listar, hasta)=>{
     try {
-        let header = "====================================================\n"
-            header += `               T A B L A   D E L  ${base}          \n`
-            header += "===================================================\n"
+        let header = "====================================================\n".green
+            header += `               T A B L A   D E L   ${base}          \n`. rainbow
+            header += "===================================================\n".green;
         
         let salida = "";
-        for(let i=1; i<=10; i++){
-            salida += `${base} X ${i} = ${base*i} \n`;
+        let salidaArchivo = "";
+        for(let i=1; i<=hasta; i++){
+            salida += `${base} ${'X'.red} ${i} ${'='.blue} ${base*i} \n`;
+            salidaArchivo += `${base} X ${i} = ${base*i} \n`;
+        }
+        dir = './salida';
+        if(!fs.existsSync(dir)){
+            fs.mkdirSync(dir);  
         }
         
-        /* fs.writeFile(`tabla-${tabla}.txt`, salida, (err)=>{
-            if(err) throw err
 
-            console.log("El archivo tabla-5.txt ha sido creado")
-        }) */
+        fs.writeFileSync(`${dir}/tabla-${base}.txt`, salidaArchivo) 
 
-        fs.writeFileSync(`tabla-${base}.txt`, salida) 
+        
             
-        return header+salida+`El archivo tabla-${base}.txt ha sido creado`    
+        return listar? header+salida+`El archivo tabla-${base}.txt ha sido creado`:`El archivo tabla-${base}.txt ha sido creado`
     } catch (error) {
         throw error
     }
